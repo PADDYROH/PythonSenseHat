017fa553adca77880570234f867005ab5f2c45c1
PADDYROH <Pat's PC@DESKTOP-PLGUNPC>
2020-03-21 18:44:16 +1100

    adding methods

--- animatedEmoji.py
+++ animatedEmoji.py
@@ -2,56 +2,62 @@
 from sense_hat import SenseHat
 import time
 
+
+
 sense=SenseHat()
 class EmojiFace:
 
     #initialize attributes
-    def init(self,emoji):
+    def __init__(self,sense,emoji):
+        self.sense = sense
         self.emoji = emoji
+    
+    def displayFace(self):
+        self.sense.set_pixels(self.emoji)
+            
+y = (255,255,0)
+b = (0,0,255)
+r = (255,0,0)
+o = (0,0,0)
+happy=[
+    o,o,o,o,o,o,o,o,
+    o,y,y,o,o,y,y,o,
+    o,y,y,o,o,y,y,o,
+    o,o,o,o,o,o,o,o,
+    o,y,o,o,o,o,y,o,
+    o,y,o,o,o,o,y,o,
+    o,o,y,y,y,y,o,o,
+    o,o,o,o,o,o,o,o,
+    ]
+        
+    
+sad=[
+    o,o,o,o,o,o,o,o,
+    o,b,b,o,o,b,b,o,
+    o,b,b,o,o,b,b,o,
+    o,o,o,o,o,o,o,o,
+    o,o,b,b,b,b,o,o,
+    o,b,o,o,o,o,b,o,
+    o,b,o,o,o,o,b,o,
+    o,o,o,o,o,o,o,o,
+    ]
+            
+meh=[
+    o,o,o,o,o,o,o,o,
+    o,r,r,o,o,r,r,o,
+    o,r,r,o,o,r,r,o,
+    o,o,o,o,o,o,o,o,
+    o,o,o,o,o,o,o,o,
+    o,r,r,r,r,r,r,o,
+    o,o,o,o,o,o,o,o,
+    o,o,o,o,o,o,o,o,
+    ]
 
-    def change(self,emoji):
-        y = (255,255,0)
-        b = (0,0,255)
-        r = (255,0,0)
-        o = (0,0,0)
-        if emoji == "happyface":
-            Img=[
-                o,o,o,o,o,o,o,o,
-                o,y,y,o,o,y,y,o,
-                o,y,y,o,o,y,y,o,
-                o,o,o,o,o,o,o,o,
-                o,y,o,o,o,o,y,o,
-                o,y,o,o,o,o,y,o,
-                o,o,y,y,y,y,o,o,
-                o,o,o,o,o,o,o,o,
-                ]
-            return Img
-        elif emoji == "sadface":
-            Img=[
-                o,o,o,o,o,o,o,o,
-                o,b,b,o,o,b,b,o,
-                o,b,b,o,o,b,b,o,
-                o,o,o,o,o,o,o,o,
-                o,o,b,b,b,b,o,o,
-                o,b,o,o,o,o,b,o,
-                o,b,o,o,o,o,b,o,
-                o,o,o,o,o,o,o,o,
-                ]
-            return Img
-        elif emoji == "mehface":
-            Img=[
-                o,o,o,o,o,o,o,o,
-                o,r,r,o,o,r,r,o,
-                o,r,r,o,o,r,r,o,
-                o,o,o,o,o,o,o,o,
-                o,o,o,o,o,o,o,o,
-                o,r,r,r,r,r,r,o,
-                o,o,o,o,o,o,o,o,
-                o,o,o,o,o,o,o,o,
-                ]
-            return Img
+e1 = EmojiFace(sense,happy)
+e2 = EmojiFace(sense,meh)
+e3 = EmojiFace(sense,sad) 
 
-e1 = EmojiFace()
-sense.set_pixels(e1.change("sadface"))
-#sense.set_pixels(e1.change("happyface"))
-#sense.set_pixels(e1.change("mehface"))
+while True:
+    e1.displayFace()
+    e2.displayFace()
+    e3.displayFace()
