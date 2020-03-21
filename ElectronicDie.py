6e01ea3c924e3dcc6831fd7bb5734dfda04604dc
PADDYROH <Pat's PC@DESKTOP-PLGUNPC>
2020-03-21 20:53:22 +1100

    changes to shake and accel

--- ElectronicDie.py
+++ ElectronicDie.py
@@ -8,28 +8,28 @@ class die:
     def __init__(self,sense):
         self.sense = sense
     
-    def roll(self):
-        acceleration = sense.get_accelerometer_raw()
+    def accel(self):
+        acceleration = self.sense.get_accelerometer_raw()
         x = acceleration['x']
         y = acceleration['y']
         z = acceleration['z']
-
-        x=round(x,0)
-        y=round(y,0)
-        z=round(z,0)
+        
+        x = abs(x)
+        y = abs(y)
+        z = abs(z)
+        
         print("x=%s, y=%s, z=%s" %(x,y,z))
         return x,y,z
     
     def shake(self,x,y,z):
-        if x !=0 or y!=0 or z!=1:
-                num = random.randint(1,7)
-                return print(num)
+        if x!=0 or y!=0 or z!=1:
+                num = random.randint(1,6)
+                self.sense.show_message(str(num))
+                return num
 
 
 d1 = die(sense)
 
 while True:
-    x,y,z = d1.roll()
-    time.sleep(1)
+    x,y,z = d1.accel()
     d1.shake(x,y,z)
-    time.sleep(1)
