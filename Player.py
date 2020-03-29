46e8bfc7e4e37ff1b031cc692f4cb634cb6eeba1
Mateo Diaz <37425648+s3708081@users.noreply.github.com>
2020-03-29 16:44:50 +1100

    Create Player.py

---
+++ Player.py
@@ -0,0 +1,20 @@
+class Player:
+
+    def __init__(self,name):
+    	self.__score = 0
+    	self.__name = name
+
+    def add_to_score(self,score_to_add):
+    	if score_to_add > 0 and score_to_add <= 6:
+    		self.__score += score_to_add
+    	else:
+    		print("Not Valid Score")
+
+    def get_score(self):
+    	return self.__score
+
+    def get_name(self):
+    	return self.__name
+
+
+
