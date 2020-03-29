27dcfaa872b3241c5a9b97957000da0c00e27c74
Mateo Diaz <37425648+s3708081@users.noreply.github.com>
2020-03-29 16:44:12 +1100

    Update monitorAndDisplay.py

--- monitorAndDisplay.py
+++ monitorAndDisplay.py
@@ -1,1 +1,65 @@
-#monitorAnddisplay
+from sense_hat import SenseHat
+import json
+import os
+import time
+
+
+class MonitorAndDisplay:
+
+    def __init__(self):
+        self.sense_hat = SenseHat()
+
+        with open('config.json') as file:
+            self.config_data = json.load(file)
+
+    def display_temp(self,temp):
+        if temp < self.config_data["cold_max"]:
+            self.sense_hat.show_message(str(temp), text_colour = MonitorAndDisplay.blue)
+        elif temp in range(self.config_data["comfortable_min"], self.config_data["comfortable_max"]):
+            self.sense_hat.show_message(str(temp), text_colour = MonitorAndDisplay.green)
+        else:
+            self.sense_hat.show_message(str(temp), text_colour = MonitorAndDisplay.red)
+
+    def get_temp(self):
+        cpu_temp = self.get_cpu_temp()
+        temp_hum = self.sense_hat.get_temperature_from_humidity()
+        temp_press = self.sense_hat.get_temperature_from_pressure()
+
+        temp = (temp_hum + temp_press) / 2
+
+        calibrated_temp = temp - ((cpu_temp - temp) / 1.5)
+
+        return int(round(calibrated_temp))
+
+    def current_temp(self,interval_to_check):
+        old_time = time.time()
+        temp = self.get_temp()
+
+        while True:
+            if time.time() - old_time > interval_to_check:
+                old_time = time.time()
+                temp = self.get_temp()
+            self.display_temp(temp)
+
+    # Get CPU temperature.
+    def get_cpu_temp(self):
+        res = os.popen("vcgencmd measure_temp").readline()
+        return float(res.replace("temp=","").replace("'C\n",""))
+
+
+    blue = (0, 0, 255)
+    green = (0, 255, 0)
+    red = (255, 0, 0)
+
+
+
+MonitorAndDisplay().current_temp(10)
+
+
+
+
+ 
+
+
+
+    
