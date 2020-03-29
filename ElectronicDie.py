114e7b9895ca48dc1763cddaf182ae81f552e1e0
Mateo Diaz <37425648+s3708081@users.noreply.github.com>
2020-03-29 16:45:15 +1100

    Update ElectronicDie.py

--- ElectronicDie.py
+++ ElectronicDie.py
@@ -1,1 +1,35 @@
 #ElectronicDie
+from sense_hat import SenseHat
+import time
+import random
+from datetime import datetime
+
+
+class die:
+    def __init__(self):
+        self.sense_hat = SenseHat()
+        self.__score = None
+    
+    def accel(self):
+        acceleration = self.sense_hat.get_accelerometer_raw()
+        x = acceleration['x']
+        y = acceleration['y']
+        z = acceleration['z']
+        
+        x = abs(x)
+        y = abs(y)
+        z = abs(z)
+        
+        return x,y,z
+    
+    def shake(self):
+        x,y,z = self.accel()
+        if x >1 or y>1 or z>1:
+                self.__scorescore = random.randrange(1,6)
+                self.sense_hat.show_message(str(self.__scorescore))
+                print ("dice rolled : " + str(self.__scorescore))
+                return self.__scorescore
+            
+die()
+
+
