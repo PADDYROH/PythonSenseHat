4790a3d294765e135757dc130909573c5cd37c26
Mateo Diaz <37425648+s3708081@users.noreply.github.com>
2020-03-29 13:16:00 +1100

    Update ElectronicDie.py

--- ElectronicDie.py
+++ ElectronicDie.py
@@ -1,19 +1,40 @@
+#ElectronicDie
 from sense_hat import SenseHat
-import json
 import time
+import random
+from datetime import datetime
+sense = SenseHat()
 
-
-class ElectronicDie:
-
-	def __init__(self):
-		self.sense_hat = SenseHat()
-
-	def 
-
-
+class die:
+    def __init__(self):
+        self.sense_hat = SenseHat()
+        while True:
+            self.shake()
 
 
     
+    def accel(self):
+        acceleration = self.sense_hat.get_accelerometer_raw()
+        x = acceleration['x']
+        y = acceleration['y']
+        z = acceleration['z']
+        
+        x = abs(x)
+        y = abs(y)
+        z = abs(z)
+        
+        return x,y,z
+    
+    def shake(self):
+        x,y,z = self.accel()
+        if x >1 or y>1 or z>1:
+                num1 = random.uniform(3,6)
+                num2 = random.uniform(1,3)
+                num = random.randint(int(num2),int(num1))
+                self.sense_hat.show_message(str(num))
+                print(num)
+                return num
 
+die()
 
 
