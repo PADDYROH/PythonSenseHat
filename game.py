fd976fc0d74fe97e49ff16c3c6292dd43dbcdfbb
PADDYROH <37425454+PADDYROH@users.noreply.github.com>
2020-03-29 16:34:08 +1100

    Update game.py

--- game.py
+++ game.py
@@ -4,85 +4,87 @@ import time
 import datetime
 import csv
 
-
 class Game:
 
-	NUM_PLAYERS = 2
-	WINING_SCORE = 4
+    NUM_PLAYERS = 2
+    WINING_SCORE = 10
 
 
 
-	def __init__(self):
-		self.__game_running = True
-		self.__players = []
-		self.winning_player = None
+    def __init__(self):
+        self.__game_running = True
+        self.__players = []
+        self.winning_player =None
+
+    def __add_players(self):
+        x = 1
+        while x <= Game.NUM_PLAYERS:
+            self.__players.append(Player(x))
+            x += 1  
+
+    def write_to_file(self,player):
+        csvWinner = open('winner.csv', 'r')
+        file_content = file.read()
+        csvWinner.close()
+        if file_content == "":
+            #Writing fileds (player, score, time) to csv file
+            fieldnames = ['player','score','time']
+            writeHeader= csv.DictWriter(csvWinner,fieldnames=fieldnames)
+            writeHeader.writeheader()
+        
+        #Writing new game winner to csv file
+        csvWinner = open('winner.csv', 'a')
+        with csvWinner:
+            writer= csv.writer(csvWinner)
+            now = datetime.now()
+            date = now.strftime("%m/%d/%y, %H:%M:%S")
+            game = [str(self.winning_player.get_name()),str(self.winning_player.get_score()), date]
+            writer.writerow(game)
+            
+
+    def check_for_winner(self):
+        winner = False
+        for player in self.__players:
+            if player.get_score() >= Game.WINING_SCORE:
+                winner = True
+                self.winning_player = player
+        if winner:
+            print(str(self.winning_player.get_name()) + " has won the game !!!")
+            self.__game_running = False
+        return winner
+
+    def play_game(self):    
+
+        print(" Welcome to the die game first to 30 wins ")
+
+        time.sleep(2)
+
+        game_dice = ElectronicDie.die()
+
+        self.__add_players()
+
+        dice_value = None
 
 
-	def __add_players(self):
-		x = 1
-		while x <= Game.NUM_PLAYERS:
-			self.__players.append(Player(x))
-			x += 1	
+        while self.__game_running:
+            for player in self.__players:
+                print("Player " + str(player.get_name()) + " : " + " it's your turn to shake the die")
+                while dice_value is None:
+                    dice_value = game_dice.shake()
 
-	def check_for_winner(self):
-		winner = False
-		for player in self.__players:
-			if player.get_score() >= Game.WINING_SCORE:
-				winner = True
-				self.winning_player = player
-		if winner:
-			print("Player " + str(self.winning_player.get_name()) + " has won the game !!!")
-			self.__game_running = False
+                player.add_to_score(dice_value)
 
-		return winner
+                print("player "+ str(player.get_name()) +" score is now " + str(player.get_score()))
 
-	def write_to_file(self):
-		csvWinner = open('winner.csv', 'a')
-		with csvWinner:
-		    #write header
-		    #fieldnames = ['player','score','time']
-		    #writeHeader= csv.DictWriter(csvWinner,fieldnames=fieldnames)
-		    #writeHeader.writeheader()
-		    #write winner
-		    writer= csv.writer(csvWinner)
-		    #player, score, time
-		    date = datetime.date.today()
-		    game = [str(self.winning_player.get_name()),str(self.winning_player.get_score()), str(date)]
-		    writer.writerow(game)
+                if self.check_for_winner():
+                    self.write_to_file(player)
+                    break
 
-	def play_game(self):	
+                time.sleep(2)
 
-		print(" Welcome to the die game first to "+ str(Game.WINING_SCORE) +" wins ")
-
-		time.sleep(2)
-
-		game_dice = ElectronicDie.die()
-
-		self.__add_players()
-
-		dice_value = None
-
-		while self.__game_running:
-			for player in self.__players:
-				print("Player " + str(player.get_name()) + " : " + " it's your turn to shake the die")
-				while dice_value is None:
-					dice_value = game_dice.shake()
-
-				player.add_to_score(dice_value)
-
-				print("player "+ str(player.get_name()) +" score is now " + str(player.get_score()))
-
-				if self.check_for_winner():
-					self.write_to_file()
-					break
-
-				time.sleep(2)
-
-				
-				dice_value = None
-
-
-
+                
+                dice_value = None
+        
 
 
 
