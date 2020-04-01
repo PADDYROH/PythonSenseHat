b0e5e1d74d5a18fbbfee5bab8b4a28859213f7c8
PADDYROH <Pat's PC@DESKTOP-PLGUNPC>
2020-04-01 13:36:23 +1100

    changes to methods

--- animatedEmoji.py
+++ animatedEmoji.py
@@ -2,29 +2,30 @@
 from sense_hat import SenseHat
 import time
 
-class EmojiFace:
-
+class Emoji:
     #initialize attributes
     def __init__(self,sense,emoji):
-        self.sense = sense
+        self.sense = SenseHat()
         self.emoji = emoji
     
+    #display on sense hat 
     def displayFace(self):
         self.sense.set_pixels(self.emoji)
 
-class Run(EmojiFace):
-    sense=SenseHat()
-
+class Face(Emoji):
+    #colours
     w = (255,255,255)
     y = (255,255,0)
-    b = (0,0,255)
     r = (255,0,0)
+    g = (0,255,0)
+    b = (0,0,255)
     o = (0,0,0)
 
+    #faces
     happy=[
         o,o,o,o,o,o,o,o,
+        o,g,w,o,o,g,w,o,
         o,w,w,o,o,w,w,o,
-        o,w,r,o,o,w,r,o,
         o,o,o,o,o,o,o,o,
         o,y,o,o,o,o,y,o,
         o,y,o,o,o,o,y,o,
@@ -44,7 +45,7 @@ class Run(EmojiFace):
         o,o,o,o,o,o,o,o,
         ]
                 
-    meh=[
+    neutral=[
         o,o,o,o,o,o,o,o,
         o,w,w,o,o,w,w,o,
         o,w,r,o,o,r,w,o,
@@ -55,12 +56,17 @@ class Run(EmojiFace):
         o,o,o,o,o,o,o,o,
         ]
 
-e1 = EmojiFace(sense,happy)
-e2 = EmojiFace(sense,meh)
-e3 = EmojiFace(sense,sad) 
+    # emojiface objects
+    emoji1 = Emoji(sense,happy)
+    emoji2 = Emoji(sense,sad)
+    emoji3 = Emoji(sense,neutral) 
 
-while True:
-    e1.displayFace()
-    e2.displayFace()
-    e3.displayFace()
+    # display objects
+    while True:
+        emoji1.displayFace()
+        time.sleep(3)
+        emoji2.displayFace()
+        time.sleep(3)
+        emoji3.displayFace()
+        time.sleep(3)
             
