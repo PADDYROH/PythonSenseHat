9808df87f49964d488e1d9867f5ef0ae5e82dfcb
Mateo Diaz <37425648+s3708081@users.noreply.github.com>
2020-04-05 14:24:55 +1000

    Update monitorAndDisplay.py

--- monitorAndDisplay.py
+++ monitorAndDisplay.py
@@ -9,8 +9,8 @@ class MonitorAndDisplay:
     def __init__(self):
         self.sense_hat = SenseHat()
 
-        with open('config.json') as file:
-            self.config_data = json.load(file)
+        # here we check if the json file is in a correct format exception is thrown otherwise
+        self.config_data = self.check_json('config.json')
 
     def display_temp(self,temp):
         if temp < self.config_data["cold_max"]:
@@ -19,6 +19,38 @@ class MonitorAndDisplay:
             self.sense_hat.show_message(str(temp), text_colour = MonitorAndDisplay.green)
         elif temp > self.config_data["hot_min"]:
             self.sense_hat.show_message(str(temp), text_colour = MonitorAndDisplay.red)
+
+    def check_json (self,json_file):
+
+        incorrect_file = False
+        cold_max = "cold_max"
+        comfortable_min = "comfortable_min"
+        comfortable_max = "comfortable_max"
+        hot_min = "hot_min"
+
+        with open(json_file) as file:
+            config_data = json.load(file)
+
+        #check if the json file is not empty
+        if os.stat(json_file).st_size == 0:
+            incorrect_file = True
+
+        # check if we can obtain the valid figures 
+        if cold_max not in config_data:
+            incorrect_file = True
+        if comfortable_min not in config_data:
+            incorrect_file = True
+        if comfortable_max  not in config_data:
+            incorrect_file = True
+        if  hot_min not in config_data:
+            incorrect_file = True  
+
+        if incorrect_file:
+            raise Exception('json file is incorrect')
+
+        else:
+            return config_data
+
 
     def get_temp(self):
         cpu_temp = self.get_cpu_temp()
@@ -33,6 +65,8 @@ class MonitorAndDisplay:
         return int(round(calibrated_temp))
 
     def current_temp(self,interval_to_check):
+        self.display_temp(self.get_temp())
+
         old_time = time.time()
         temp = self.get_temp()
 
@@ -40,8 +74,8 @@ class MonitorAndDisplay:
             #if the current time is greater then the interval to check (10 seconds)
             if time.time() - old_time > interval_to_check:
                 old_time = time.time()
-                temp = self.get_temp()
-            self.display_temp(temp)
+                self.display_temp(self.get_temp())
+
 
     # Get CPU temperature.
     def get_cpu_temp(self):
@@ -56,12 +90,3 @@ class MonitorAndDisplay:
 
 
 MonitorAndDisplay().current_temp(10)
-
-
-
-
- 
-
-
-
-    
