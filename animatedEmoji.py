02512b4140254230c1258257a12222cb346f82f4
Mateo Diaz <37425648+s3708081@users.noreply.github.com>
2020-04-05 12:37:35 +1000

    Update animatedEmoji.py

--- animatedEmoji.py
+++ animatedEmoji.py
@@ -1,20 +1,74 @@
-import Emoji
+#animatedEmoji
+from sense_hat import SenseHat
 import time
 
+class Emoji:
+    #initialize attributes
+    def __init__(self,emoji):
+        self.sense = SenseHat()
+        self.emoji = emoji
+    
+    #display on sense hat 
+    def set_new_emoji(self):
+        self.sense.set_pixels(self.emoji)
 
-from sense_hat import SenseHat
+class Face:
 
-sense = SenseHat()
+    #colours
+    w = (255,255,255)
+    y = (255,255,0)
+    r = (255,0,0)
+    g = (0,255,0)
+    b = (0,0,255)
+    o = (0,0,0)
 
-emoji = Emoji.Emoji(sense)
+#faces
+    happy=[
+        o,o,o,o,o,o,o,o,
+        o,g,w,o,o,g,w,o,
+        o,w,w,o,o,w,w,o,
+        o,o,o,o,o,o,o,o,
+        o,y,o,o,o,o,y,o,
+        o,y,o,o,o,o,y,o,
+        o,o,y,y,y,y,o,o,
+        o,o,o,o,o,o,o,o,
+        ]
+                
+    sad=[
+        o,o,o,o,o,o,o,o,
+        o,w,b,o,o,w,b,o,
+        o,w,w,o,o,w,w,o,
+        o,o,o,o,o,o,o,o,
+        o,o,y,y,y,y,o,o,
+        o,y,o,o,o,o,y,o,
+        o,y,o,o,o,o,y,o,
+        o,o,o,o,o,o,o,o,
+        ]
+                   
+    neutral=[
+        o,o,o,o,o,o,o,o,
+        o,w,w,o,o,w,w,o,
+        o,w,r,o,o,r,w,o,
+        o,o,o,o,o,o,o,o,
+        o,o,o,o,o,o,o,o,
+        o,y,y,y,y,y,y,o,
+        o,o,o,o,o,o,o,o,
+        o,o,o,o,o,o,o,o,
+        ]
+          
+    def display_emojis(self):
+    
+        emoji1 = Emoji(self.happy)
+        emoji2 = Emoji(self.sad)
+        emoji3 = Emoji(self.neutral) 
 
+            # display objects
+        while True:
+            emoji1.set_new_emoji()
+            time.sleep(3)
+            emoji2.set_new_emoji()
+            time.sleep(3)
+            emoji3.set_new_emoji()
+            time.sleep(3)
 
-while 1 :
-	emoji.display_emoji("penis")
-	time.sleep(3)
-	emoji.display_emoji("invader")
-	time.sleep(3)
-	emoji.display_emoji("frown")
-	time.sleep(3)
-
-
+Face().display_emojis()
